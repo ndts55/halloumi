4ecbb50e7d94e5153d701b199ca4eafd110c0aa8
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-07-02 15:43:02 +0200

    chore: init

---
+++ include/kernel.cuh
@@ -0,0 +1,7 @@
+#pragma once
+
+__global__ void advance_step();
+
+__global__ void evaluate_ode();
+
+__global__ void all_terminated();
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+int main(int argc, char const *argv[])
+{
+    // TODO read in ephemeris input data
+    // TODO read in sample input data
+    // TODO setup the simulation for CUDA devices
+    // TODO ensure coalesced access to data in global memory
+    // TODO implement kernel for ode evaluation (all stages)
+    // TODO implement kernel for step advancement (adaptive step size)
+    // TODO implement kernel for determining whether all samples have finished
+    // TODO ensure simulation is stopped if number of steps exceed given value
+    // TODO run the simulation on CUDA device
+    // TODO get output data from device
+    // TODO write output data to some output file for easy comparison
+    // TODO implement mathematical model
+    // TODO implement physical model
+    // TODO implement coalesced access to current states
+    // TODO implement ode evaluation stages as for loop
+    // TODO test register count for unrolled / not unrolled for loop
+    return 0;
+}
