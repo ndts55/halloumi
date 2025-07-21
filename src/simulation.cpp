086fd4e0be8c7c3050c7647d6a6654025bfd21fc
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-07-21 21:28:02 +0200

    feat: changed simulation to struct

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -3,19 +3,19 @@
 #include "types.hpp"
 #include "simulation.hpp"
 #include "utils.hpp"
-#include "samples.hpp"
+#include "propagation_context.hpp"
 #include "ephemeris.hpp"
 
 Simulation Simulation::from_json(const nlohmann::json &configuration)
 {
     Float duration_in_days = configuration["simConfig"]["durationDays"];
 
-    auto samples_json = json_from_file(configuration["samples"]);
-    auto samples = Samples::from_json(samples_json);
+    auto propagation_context = PropagationContext::from_json(configuration);
 
     auto exec_config = json_from_file(configuration["simConfig"]["config"]);
     auto brie_file = exec_config["model"]["environment"]["ephemeris"];
     auto ephemeris = Ephemeris::from_brie(brie_file);
+    auto rkf_params = RKFParameters::from_json(exec_config["integration"]);
 
-    return Simulation(duration_in_days, std::move(samples), std::move(ephemeris));
+    return Simulation{std::move(propagation_context), std::move(ephemeris), std::move(rkf_params)};
 }
