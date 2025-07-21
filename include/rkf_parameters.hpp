65d63225e01479cb2ff147ef6d12ad38e3b09923
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-07-21 21:27:10 +0200

    feat: added RKF parameters struct to hold some numbers

---
+++ include/rkf_parameters.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "types.hpp"
+#include <nlohmann/json.hpp>
+#include <cstddef>
+#include <cuda_runtime.h>
+
+struct RKFParameters
+{
+    Float initial_time_step = 1;
+    Float min_time_step = 1e-6;
+    Float abs_tol = 1e-9;
+    Float rel_tol = 1e-9;
+    Float scale_state = 1;
+    Float scale_dstate = 0;
+    std::size_t max_steps = 1e6;
+
+    static constexpr Float dt_safety = .9;
+    static constexpr Float min_dt_scale = .2;
+    static constexpr Float max_dt_scale = 5;
+
+    __host__ static RKFParameters from_json(const nlohmann::json &exec_json);
+};
