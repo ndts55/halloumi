39d32d2d29cfa3f137eaee13d2a543ac109fed36
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-08-05 13:11:12 +0200

    feat: added + and - ops to vec

--- include/cuda/vec.cuh
+++ include/cuda/vec.cuh
@@ -20,4 +20,58 @@ struct Vec
     {
         return N;
     }
+
+    __host__ __device__ Vec<T, N> operator+(const Vec<T, N> &other) const
+    {
+        Vec<T, N> result;
+        for (std::size_t i = 0; i < N; ++i)
+        {
+            result[i] = data[i] + other[i];
+        }
+        return result;
+    }
+
+    __host__ __device__ Vec<T, N> operator-(const Vec<T, N> &other) const
+    {
+        Vec<T, N> result;
+        for (std::size_t i = 0; i < N; ++i)
+        {
+            result[i] = data[i] - other[i];
+        }
+        return result;
+    }
+
+    __host__ __device__ Vec<T, N> &operator+=(const Vec<T, N> &other)
+    {
+        for (std::size_t i = 0; i < N; ++i)
+            data[i] += other.data[i];
+        return *this;
+    }
+
+    __host__ __device__ Vec<T, N> &operator-=(const Vec<T, N> &other)
+    {
+        for (std::size_t i = 0; i < N; ++i)
+            data[i] -= other.data[i];
+        return *this;
+    }
+
+    __host__ __device__ inline T *begin()
+    {
+        return data;
+    }
+
+    __host__ __device__ inline T *end()
+    {
+        return data + N;
+    }
+
+    __host__ __device__ inline const T *cbegin() const
+    {
+        return data;
+    }
+
+    __host__ __device__ inline const T *cend() const
+    {
+        return data + N;
+    }
 };
