e57b07391a0ff04c01a2cbe8d7759f19bd54011f
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-08-05 19:41:53 +0200

    chore: define dims and offset for state vector depending on STATE_DIM

--- include/core/types.cuh
+++ include/core/types.cuh
@@ -3,10 +3,10 @@
 #pragma region State Vector Dimensions
 
 #define STATE_DIM 6
-#define POSITION_DIM 3
+#define POSITION_DIM (STATE_DIM / 2)
 #define POSITION_OFFSET 0
-#define VELOCITY_DIM 3
-#define VELOCITY_OFFSET 3
+#define VELOCITY_DIM (POSITION_DIM)
+#define VELOCITY_OFFSET (POSITION_DIM + POSITION_OFFSET)
 
 #pragma endregion
 
