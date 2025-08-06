33a932b40061456946641739f36a9cd7593e247f
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-08-06 15:50:28 +0200

    fix: use device ephemeris position calculation methods

--- src/simulation/propagation/propagate.cu
+++ src/simulation/propagation/propagate.cu
@@ -13,60 +13,6 @@
 
 #pragma region Math and Physics Helpers
 
-__device__ Vec<Float, POSITION_DIM> interpolate_type_2_body_to_position(const DeviceEphemeris &eph, const Integer &body_index, const Float &epoch)
-{
-    auto nintervals = eph.nintervals_at(body_index);
-    auto data_offset = eph.dataoffset_at(body_index);
-    auto pdeg = eph.pdeg_at(body_index);
-
-    // data = [ ...[other data; (data_offset)], interval radius, ...[intervals; (nintervals)], ...[coefficients; (nintervals * (pdeg + 1))] ]
-    auto radius = eph.data_at(data_offset);
-    DeviceArray1D<Float> intervals{/* data pointer */ eph.data.data + data_offset + 1, /* size */ (std::size_t)nintervals};
-    DeviceArray1D<Float> coefficients{/* data pointer */ intervals.end(), /* size */ (std::size_t)nintervals * (pdeg + 1)};
-
-    std::size_t idx = (epoch - intervals.at(0)) / (2 * radius);
-    Float s = (epoch - intervals.at(idx)) / radius - 1.0;
-    Vec<Float, POSITION_DIM> position = {0.0};
-    Vec<Float, POSITION_DIM> w1 = {0.0};
-    Vec<Float, POSITION_DIM> w2 = {0.0};
-    Float s2 = 2 * s;
-    for (auto i = pdeg; i > 0; --i)
-    {
-        w2 = w1;
-        w1 = position;
-        position = (w1 * s2 - w2) + coefficients.at(i * nintervals + idx);
-    }
-    return (position * s - w1) + coefficients.at(idx);
-}
-
-__device__ Vec<Float, POSITION_DIM> read_position(const DeviceEphemeris &eph, const Float &epoch, const Integer &target, const Integer &center)
-{
-    Vec<Float, POSITION_DIM> position = {0.0};
-    if (target == center)
-    {
-        return position;
-    }
-
-    Integer t = target;
-    while (t != center)
-    {
-        auto body_index = eph.index_of_target(t);
-        // ! We only have type 2 bodies for now.
-        position += interpolate_type_2_body_to_position(eph, body_index, epoch);
-        t = eph.center_at(body_index);
-    }
-    return position;
-}
-
-__device__ Vec<Float, POSITION_DIM> calculate_position(const DeviceEphemeris &eph, const Float &epoch, const Integer &target, const Integer &center_of_integration)
-{
-    auto cc = eph.common_center(target, center_of_integration);
-    auto xt = read_position(eph, epoch, target, cc);
-    auto xc = read_position(eph, epoch, center_of_integration, cc);
-    xt -= xc;
-    return xt;
-}
-
 __device__ Vec<Float, STATE_DIM> calculate_current_state(
     const int &stage,
     const DeviceArray2D<Float, STATE_DIM> &states,
@@ -97,21 +43,24 @@ __device__ Vec<Float, STATE_DIM> calculate_current_state(
     return state;
 }
 
-__device__ Vec<Float, VELOCITY_DIM> calculate_velocity_delta(
+__device__ Vec<Float, STATE_DIM> calculate_state_delta(
     const DeviceConstants &constants,
     const DeviceEphemeris &ephemeris,
-    const Integer &epoch,
     DeviceArray1D<Integer> &active_bodies,
     const Integer &center_of_integration,
-    const Vec<Float, POSITION_DIM> &state_position)
+    const Integer &t,
+    const Vec<Float, STATE_DIM> &state)
 {
-    Vec<Float, VELOCITY_DIM> vec{0.0};
+    const auto state_position = state.slice<POSITION_OFFSET, POSITION_DIM>();
+
+    // velocity delta, i.e., acceleration
+    Vec<Float, VELOCITY_DIM> velocity_delta{0.0};
     if (center_of_integration == 0)
     {
         for (const auto target : active_bodies)
         {
-            auto body_position = calculate_position(ephemeris, epoch, target, center_of_integration);
-            vec += three_body_barycentric(state_position, body_position, constants.gm_for(target));
+            auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
+            velocity_delta += three_body_barycentric(state_position, body_position, constants.gm_for(target));
         }
     }
     else
@@ -120,29 +69,16 @@ __device__ Vec<Float, VELOCITY_DIM> calculate_velocity_delta(
         {
             if (target != center_of_integration)
             {
-                auto body_position = calculate_position(ephemeris, epoch, target, center_of_integration);
-                vec += three_body_non_barycentric(state_position, body_position, constants.gm_for(target));
+                auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
+                velocity_delta += three_body_non_barycentric(state_position, body_position, constants.gm_for(target));
             }
             else
             {
                 auto gm = constants.gm_for(target);
-                vec += two_body(state_position, gm);
+                velocity_delta += two_body(state_position, gm);
             }
         }
     }
-    return vec;
-}
-
-__device__ Vec<Float, STATE_DIM> evaluate_ode_for_stage(
-    const DeviceConstants &constants,
-    const DeviceEphemeris &ephemeris,
-    DeviceArray1D<Integer> &active_bodies,
-    const Integer &center_of_integration,
-    const Integer &t,
-    const Vec<Float, STATE_DIM> &state)
-{
-    // velocity delta, i.e., acceleration
-    auto velocity_delta = calculate_velocity_delta(constants, ephemeris, t, active_bodies, center_of_integration, state.slice<POSITION_OFFSET, POSITION_DIM>());
     // Velocity of previous state becomes position delta
     return state.slice<VELOCITY_OFFSET, VELOCITY_DIM>().append(velocity_delta);
 }
@@ -196,11 +132,17 @@ __global__ void evaluate_ode(
     const auto dt = next_dts.at(index);
     const auto epoch = epochs.at(index);
 
+    // TODO optimization for stage = 0 where no call to calculate_current_state or RKF78::node is needed
     for (auto stage = 0; stage < RKF78::NStages; ++stage)
     {
         const auto current_state = calculate_current_state(stage, states, d_states, index, dt);
-        const Float t = epoch + RKF78::node(stage) * dt;
-        auto state_delta = evaluate_ode_for_stage(constants, ephemeris, active_bodies, center_of_integration, t, current_state);
+        auto state_delta = calculate_state_delta(
+            constants,
+            ephemeris,
+            active_bodies,
+            center_of_integration,
+            /* t */ epoch + RKF78::node(stage) * dt,
+            current_state);
         for (auto dim = 0; dim < STATE_DIM; ++dim)
         {
             d_states.at(dim, stage, index) = state_delta[dim];
