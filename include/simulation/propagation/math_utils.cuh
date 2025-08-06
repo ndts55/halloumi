768c807781e140aae6cbed27e5c45b2980fb242f
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-08-06 16:26:33 +0200

    chore: moved math utils to math utils cuh

--- include/simulation/propagation/math_utils.cuh
+++ include/simulation/propagation/math_utils.cuh
@@ -1,6 +1,10 @@
 #include <cuda_runtime.h>
 #include "cuda/vec.cuh"
 #include "core/types.cuh"
+#include "simulation/tableau.cuh"
+#include "cuda/device_array.cuh"
+#include "simulation/environment/ephemeris.cuh"
+#include "simulation/environment/constants.cuh"
 
 __device__ inline Vec<Float, VELOCITY_DIM> two_body(const Vec<Float, POSITION_DIM> &position_delta, const Float &gm)
 {
@@ -15,4 +19,80 @@ __device__ inline Vec<Float, VELOCITY_DIM> three_body_barycentric(const Vec<Floa
 __device__ inline Vec<Float, VELOCITY_DIM> three_body_non_barycentric(const Vec<Float, POSITION_DIM> &source_position, const Vec<Float, POSITION_DIM> &body_position, const Float &gm)
 {
     return three_body_barycentric(source_position, body_position, gm) + two_body(body_position, gm);
+}
+
+__device__ Vec<Float, STATE_DIM> calculate_current_state(
+    // State data
+    const DeviceArray2D<Float, STATE_DIM> &states,
+    const DeviceArray3D<Float, STATE_DIM, RKF78::NStages> &d_states,
+    // Computation coordinates
+    const CudaIndex &index,
+    const int &stage,
+    const Float &dt)
+{
+    Vec<Float, STATE_DIM> state = {0.0};
+
+    // sum intermediate d_states up to stage
+    for (auto st = 0; st < stage; ++st)
+    {
+        auto coefficient = RKF78::coefficient(stage, st);
+        // state += coefficient * d_states.at(st, index)
+        for (auto dim = 0; dim < STATE_DIM; ++dim)
+        {
+            state[dim] += coefficient * d_states.at(dim, st, index);
+        }
+    }
+
+    // add the current state
+    // state = states.at(index) + dt * state
+    for (auto dim = 0; dim < STATE_DIM; ++dim)
+    {
+        state[dim] *= dt;
+        state[dim] += states.at(dim, index);
+    }
+    return state;
+}
+
+__device__ Vec<Float, STATE_DIM> calculate_state_delta(
+    // Primary inputes
+    const Vec<Float, STATE_DIM> &state,
+    const Float &t,
+    // Physics configs
+    const Integer &center_of_integration,
+    const DeviceArray1D<Integer> &active_bodies,
+    const DeviceConstants &constants,
+    const DeviceEphemeris &ephemeris)
+{
+    const auto state_position = state.slice<POSITION_OFFSET, POSITION_DIM>();
+
+    // velocity delta, i.e., acceleration
+    Vec<Float, VELOCITY_DIM> velocity_delta{0.0};
+    if (center_of_integration == 0)
+    {
+        for (auto i = 0; i < active_bodies.n_vecs; ++i)
+        {
+            const auto target = active_bodies.at(i);
+            const auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
+            velocity_delta += three_body_barycentric(state_position, body_position, constants.gm_for(target));
+        }
+    }
+    else
+    {
+        for (auto i = 0; i < active_bodies.n_vecs; ++i)
+        {
+            const auto target = active_bodies.at(i);
+            if (target != center_of_integration)
+            {
+                const auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
+                velocity_delta += three_body_non_barycentric(state_position, body_position, constants.gm_for(target));
+            }
+            else
+            {
+                const auto gm = constants.gm_for(target);
+                velocity_delta += two_body(state_position, gm);
+            }
+        }
+    }
+    // Velocity of previous state becomes position delta
+    return state.slice<VELOCITY_OFFSET, VELOCITY_DIM>().append(velocity_delta);
 }
--- src/simulation/propagation/propagate.cu
+++ src/simulation/propagation/propagate.cu
@@ -11,86 +11,6 @@
 #include "simulation/propagation/cuda_utils.cuh"
 #include "simulation/propagation/math_utils.cuh"
 
-#pragma region Math and Physics Helpers
-
-__device__ Vec<Float, STATE_DIM> calculate_current_state(
-    // State data
-    const DeviceArray2D<Float, STATE_DIM> &states,
-    const DeviceArray3D<Float, STATE_DIM, RKF78::NStages> &d_states,
-    // Computation coordinates
-    const CudaIndex &index,
-    const int &stage,
-    const Float &dt)
-{
-    Vec<Float, STATE_DIM> state = {0.0};
-
-    // sum intermediate d_states up to stage
-    for (auto st = 0; st < stage; ++st)
-    {
-        auto coefficient = RKF78::coefficient(stage, st);
-        // state += coefficient * d_states.at(st, index)
-        for (auto dim = 0; dim < STATE_DIM; ++dim)
-        {
-            state[dim] += coefficient * d_states.at(dim, st, index);
-        }
-    }
-
-    // add the current state
-    // state = states.at(index) + dt * state
-    for (auto dim = 0; dim < STATE_DIM; ++dim)
-    {
-        state[dim] *= dt;
-        state[dim] += states.at(dim, index);
-    }
-    return state;
-}
-
-__device__ Vec<Float, STATE_DIM> calculate_state_delta(
-    // Primary inputes
-    const Vec<Float, STATE_DIM> &state,
-    const Float &t,
-    // Physics configs
-    const Integer &center_of_integration,
-    const DeviceArray1D<Integer> &active_bodies,
-    const DeviceConstants &constants,
-    const DeviceEphemeris &ephemeris)
-{
-    const auto state_position = state.slice<POSITION_OFFSET, POSITION_DIM>();
-
-    // velocity delta, i.e., acceleration
-    Vec<Float, VELOCITY_DIM> velocity_delta{0.0};
-    if (center_of_integration == 0)
-    {
-        for (auto i = 0; i < active_bodies.n_vecs; ++i)
-        {
-            const auto target = active_bodies.at(i);
-            const auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
-            velocity_delta += three_body_barycentric(state_position, body_position, constants.gm_for(target));
-        }
-    }
-    else
-    {
-        for (auto i = 0; i < active_bodies.n_vecs; ++i)
-        {
-            const auto target = active_bodies.at(i);
-            if (target != center_of_integration)
-            {
-                const auto body_position = ephemeris.calculate_position(t, target, center_of_integration);
-                velocity_delta += three_body_non_barycentric(state_position, body_position, constants.gm_for(target));
-            }
-            else
-            {
-                const auto gm = constants.gm_for(target);
-                velocity_delta += two_body(state_position, gm);
-            }
-        }
-    }
-    // Velocity of previous state becomes position delta
-    return state.slice<VELOCITY_OFFSET, VELOCITY_DIM>().append(velocity_delta);
-}
-
-#pragma endregion
-
 #pragma region Kernels
 
 __global__ void prepare_simulation_run(
@@ -126,13 +46,10 @@ __global__ void evaluate_ode(
     const DeviceArray2D<Float, STATE_DIM> states,
     const DeviceArray1D<Float> epochs,
     const DeviceArray1D<Float> next_dts,
-
     // Output data
     DeviceArray3D<Float, STATE_DIM, RKF78::NStages> d_states,
-
     // Control flags
     const DeviceArray1D<bool> termination_flags,
-
     // Physics configs
     const Integer center_of_integration,
     const DeviceArray1D<Integer> active_bodies,
