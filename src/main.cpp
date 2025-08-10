9563ec4b5a4e577d35b041c2f5cfa177c2f2d082
Nicolas Torres <nicolas_daniel.torres@stud.tu-darmstadt.de>
2025-08-10 17:18:42 +0200

    chore: update main to just call propagate on simulation

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,17 @@
 #include <cuda_runtime.h>
 #include "utils.hpp"
 #include "simulation/simulation.hpp"
-#include "simulation/propagate.cuh"
+#include "simulation/propagation/propagate.cuh"
 
 // TODO take config file as command line argument
-// TODO ensure simulation is stopped if number of steps exceed given value
 // TODO get output data from device
 // TODO write output data to some output file for easy comparison
-// TODO implement mathematical model
-// TODO implement physical model
-// TODO implement ode evaluation stages as for loop
 // TODO test register count for unrolled / not unrolled for loop
 
 int main()
 {
     auto configuration = json_from_file("acceptance/acceptance.test.5-days.json");
     auto simulation = Simulation::from_json(configuration);
-
-    std::cout << "Read environment\n"
-              << "Ephemeris Bodies: "
-              << simulation.ephemeris.n_bodies() << "\n"
-              << "Constants Bodies: "
-              << simulation.constants.n_bodies() << "\n"
-              << std::endl;
-    std::cout << "Read propagation context\n"
-              << "States: "
-              << simulation.propagation_context.samples_data.n_vecs << std::endl;
-    std::cout << "Read RKF parameters\n"
-              << "> abs tol: " << simulation.rkf_parameters.abs_tol << "\n"
-              << "> rel tol: " << simulation.rkf_parameters.rel_tol << "\n"
-              << "> initial time step: " << simulation.rkf_parameters.initial_time_step << "\n"
-              << "> min time step: " << simulation.rkf_parameters.min_time_step << "\n"
-              << "> max steps: " << simulation.rkf_parameters.max_steps << std::endl;
 
     // Launch test kernel to verify device memory setup
     std::cout << "Propagating" << std::endl;
